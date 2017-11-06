c48ac8d5dea8dbb9c19ee4e0fa62dc2f0c615b14
Ku Lok Sun <sundaykuloksun@gmail.com>
2017-11-06 17:38:59 +0800

    Update the cask packages

--- install.sh
+++ install.sh
@@ -6,14 +6,23 @@ xcode-select -p
 # install brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+
+
 # brew cask
 brew cask
+# browser
+brew cask install \
+    google-chrome \
+    firefox
+# Utils
+brew cask install \
+    alfred \
+    clipy \
+    caffeine    
+    spectacle
+# Dev Tools
 brew cask install \
     iterm2 \
-    google-chrome \
-    firefox \
-    bitbar \
     sequel-pro \
-    dockertoolbox \
-    alfred \
-    clipmenu
+    docker \
+    sublime-text
